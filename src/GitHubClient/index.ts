e88e48a870b53068f4c2777a5ae0d8257701d03c
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-10-18 20:42:25 +0300

    Exported './Types'.

--- src/GitHubClient/index.ts
+++ src/GitHubClient/index.ts
@@ -1,2 +1,11 @@
 export { GitHubClient } from './GitHubClient';
 export { IGitHubClient } from './IGitHubClient';
+export {
+  Branch,
+  Commit,
+  File,
+  Milestone,
+  PullRequest,
+  PullRequestStatus,
+  User,
+} from './Types';
