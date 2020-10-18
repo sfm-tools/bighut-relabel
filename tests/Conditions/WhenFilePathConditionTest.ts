5ea48e732fa8fa1f27f2c53b216d96826523795b
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-10-19 02:02:38 +0300

    Created test for WhenFilePathCondition. This is just the beginning. Much more tests are required.

---
+++ tests/Conditions/WhenFilePathConditionTest.ts
@@ -0,0 +1,52 @@
+import { expect } from 'chai';
+
+import { WhenFilePathConditionOptions } from '../../src/ConditionOptions';
+import { WhenFilePathCondition } from '../../src/Conditions';
+import { LabelerContext } from '../../src/LabelerContext';
+import { pullRequests } from '../Resources';
+
+describe('WhenFilePathCondition', () => {
+  const context = new LabelerContext(pullRequests[0]);
+
+  it('should return true for an existing file using a string predicate', async(): Promise<void> => {
+    const when = new WhenFilePathCondition('Frontend/package.json');
+    const result = await when.test(context);
+
+    expect(true).to.equal(result);
+  });
+
+  it('should return true for an existing in list file using a function predicate', async(): Promise<void> => {
+    const when = new WhenFilePathCondition((filePath: string): boolean => (
+      filePath.includes('webpack.js')
+    ));
+
+    const result = await when.test(context);
+
+    expect(true).to.equal(result);
+  });
+
+  it('should return false for a removed file with excludeRemovedFiles option', async(): Promise<void> => {
+    const options = new WhenFilePathConditionOptions(null);
+    const when = new WhenFilePathCondition((filePath: string): boolean => (
+      filePath.includes('webpack.js')
+    ), options);
+
+    options.excludeRemovedFiles();
+
+    const result = await when.test(context);
+
+    expect(false).to.equal(result);
+  });
+
+  it('should return false for an existing file that was excluded using excludeFilePath option', async(): Promise<void> => {
+    const options = new WhenFilePathConditionOptions(null);
+    const when = new WhenFilePathCondition(/.json/g, options);
+
+    options.excludeFilePath(/package.json/g);
+
+    const result = await when.test(context);
+
+    expect(false).to.equal(result);
+  });
+
+});
