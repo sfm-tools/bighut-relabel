bac871b8fb18ea884528e2a4599bbc0add67e52f
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-10-23 00:29:43 +0300

    Created tests for ExecuteActionExecutor.

---
+++ tests/ActionExecutors/ExecuteActionExecutorTest.ts
@@ -0,0 +1,40 @@
+import { expect } from 'chai';
+
+import { ExecuteActionExecutor } from '../../src/ActionExecutors';
+import { ExecuteAction } from '../../src/Actions';
+import { LabelerContext } from '../../src/LabelerContext';
+import { flossTomUser,pullRequests } from '../Resources';
+
+describe('ExecuteActionExecutor', () => {
+
+  it('should add a task to the updater', async(): Promise<void> => {
+    const context = new LabelerContext(pullRequests[0]);
+    const updater = context.updater;
+    const action = new ExecuteAction((): void => { });
+    const executor = new ExecuteActionExecutor(action);
+
+    action
+      .whenAuthorLogin(flossTomUser.login);
+
+    await executor.execute(context);
+
+    expect(Array.from(updater.tasks)).to.deep.equal([action.execute]);
+  });
+
+  it('should not add any tasks to the updater', async(): Promise<void> => {
+    const context = new LabelerContext(pullRequests[0]);
+    const updater = context.updater;
+    const action = new ExecuteAction((): void => { });
+    const executor = new ExecuteActionExecutor(action);
+
+    action
+      .whenAuthorLogin(flossTomUser.login)
+      .andAlso()
+      .whenDescription('abracadabra');
+
+    await executor.execute(context);
+
+    expect(Array.from(updater.tasks)).to.deep.equal([]);
+  });
+
+});
