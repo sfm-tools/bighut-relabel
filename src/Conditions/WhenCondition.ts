6e2c52a7886fca1ab1519b7348266339e1864513
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-10-22 21:05:18 +0300

    WhenCondition - NotSupportedParameterError('noOne').

--- src/Conditions/WhenCondition.ts
+++ src/Conditions/WhenCondition.ts
@@ -1,3 +1,4 @@
+import { NotSupportedParameterError } from '../Errors';
 import { LabelerContext } from '../LabelerContext';
 import { BaseCondition } from './BaseCondition';
 import { WhenConditionPredicate } from './Types';
@@ -5,6 +6,14 @@ import { WhenConditionPredicate } from './Types';
 export class WhenCondition extends BaseCondition<WhenConditionPredicate> {
 
   public test(context: LabelerContext): boolean {
+    const {
+      noOne,
+    } = this.getOptions();
+
+    if (noOne) {
+      throw new NotSupportedParameterError('noOne');
+    }
+
     return this.predicate(context);
   }
 
