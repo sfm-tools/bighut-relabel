24ad6cf69d8f3a85e7ef5c820e422f51eba1300d
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-10-22 21:05:52 +0300

    Improved tests for WhenCondition.

--- tests/Conditions/WhenConditionTest.ts
+++ tests/Conditions/WhenConditionTest.ts
@@ -1,6 +1,8 @@
 import { expect } from 'chai';
 
+import { DefaultConditionOptions } from '../../src/ConditionOptions';
 import { WhenCondition } from '../../src/Conditions';
+import { NotSupportedParameterError } from '../../src/Errors';
 import { LabelerContext } from '../../src/LabelerContext';
 import { pullRequests } from '../Resources';
 
@@ -13,7 +15,20 @@ describe('WhenCondition', () => {
 
     const context = new LabelerContext(pullRequests[0]);
 
-    expect(true).equal(when.test(context));
+    expect(true).to.equal(when.test(context));
+  });
+
+  it('an exception should be thrown', (): void => {
+    const options = new DefaultConditionOptions(null);
+    const when = new WhenCondition((context: LabelerContext): boolean => {
+      return !!context.pullRequest;
+    }, options);
+
+    options.noOne();
+
+    const context = new LabelerContext(pullRequests[0]);
+
+    expect(() => when.test(context)).to.throw(new NotSupportedParameterError('noOne').message);
   });
 
 });
