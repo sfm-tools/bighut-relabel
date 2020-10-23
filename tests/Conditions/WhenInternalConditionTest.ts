4ebe803ef2345e467b54718f770d1d069a8a1993
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-10-23 23:53:53 +0300

    Created tests for WhenInternalCondition.

---
+++ tests/Conditions/WhenInternalConditionTest.ts
@@ -0,0 +1,32 @@
+import { expect } from 'chai';
+
+import { WhenInternalConditionOptions } from '../../src/ConditionOptions';
+import { WhenInternalCondition } from '../../src/Conditions';
+import { LabelerContext } from '../../src/LabelerContext';
+import { pullRequests } from '../Resources';
+
+describe('WhenInternalCondition', () => {
+
+  it('should be enabled the stopped option in the context instance', (): void => {
+    const options = new WhenInternalConditionOptions(null);
+    const when = new WhenInternalCondition(options);
+    const context = new LabelerContext(pullRequests[0]);
+
+    options.ignoreOtherActions();
+
+    when.test(context);
+
+    expect(context.stopped).to.be.true;
+  });
+
+  it('should not be enabled the stopped option in the context instance', (): void => {
+    const options = new WhenInternalConditionOptions(null);
+    const when = new WhenInternalCondition(options);
+    const context = new LabelerContext(pullRequests[0]);
+
+    when.test(context);
+
+    expect(context.stopped).to.be.false;
+  });
+
+});
