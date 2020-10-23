10ca8d3ba80536a1782593f81177d1332592bd35
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-10-24 01:47:32 +0300

    Config - Added addLabel method.

--- src/Config.ts
+++ src/Config.ts
@@ -1,8 +1,10 @@
 import {
+  AddLabelActionExecutor,
   BaseActionExecutor,
   ExecuteActionExecutor,
 } from './ActionExecutors';
 import {
+  AddLabelAction,
   BaseAction,
   ExecuteAction,
 } from './Actions';
@@ -10,6 +12,14 @@ import {
 export class Config {
 
   private _actions = new Array<BaseActionExecutor<BaseAction>>();
+
+  public addLabel(label: string): AddLabelAction {
+    return this.addAction(
+      new AddLabelActionExecutor(
+        new AddLabelAction(label)
+      )
+    );
+  }
 
   public execute(action: VoidFunction): ExecuteAction {
     return this.addAction(
