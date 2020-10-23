e4db1cff9cf827283e00eea71d5605e965914830
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-10-24 00:41:18 +0300

    pullRequests - Added pull request containing conflicts.

--- tests/Resources/PullRequests.ts
+++ tests/Resources/PullRequests.ts
@@ -211,4 +211,52 @@ export const pullRequests: Array<PullRequest> = [
       commits: 3,
     }),
   },
+  {
+    author: loftMossUser,
+    code: 2,
+    comments: Promise.resolve<Array<Comment>>([]),
+    commits: Promise.resolve<Array<Commit>>([
+      {
+        author: loftMossUser,
+        hash: '1121a3ee5e6b5b0d3255bfef95601890afd80701',
+        message: 'Updated README.md',
+      },
+    ]),
+    description: null,
+    files: Promise.resolve<Array<File>>([
+      {
+        additions: 451324,
+        changes: 1233324,
+        deletions: 133,
+        content: Promise.resolve('Awesome README'),
+        filePath: 'README.md',
+        patch: null,
+        status: 'modified',
+      },
+    ]),
+    htmlUrl: 'https://github.com/sfm-tools/bighut-relabel/pulls/2',
+    id: 999000902,
+    labels: null,
+    milestone: null,
+    sourceBranch: {
+      name: 'issue-2',
+      isExists: Promise.resolve(true),
+    },
+    targetBranch: {
+      name: 'main',
+      isExists: Promise.resolve(true),
+    },
+    state: 'open',
+    title: 'Readme fixes',
+    createdDate: new Date(2020, 9, 24, 0, 30, 0),
+    statusInfo: Promise.resolve({
+      merged: false,
+      mergeable: true,
+      mergeableState: 'dirty',
+      rebaseable: true,
+      changedFiles: 1,
+      comments: 0,
+      commits: 1,
+    }),
+  }
 ];
