30b97744d3d171e43fa8241c98dc6d1a7f8aa60b
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-10-24 00:15:32 +0300

    PullRequestStatus - Extended info.

--- src/GitHubClient/Types/PullRequestStatus.ts
+++ src/GitHubClient/Types/PullRequestStatus.ts
@@ -2,10 +2,19 @@ export type PullRequestStatus = {
 
   merged: boolean;
 
-  mergeable: boolean;
+  /**
+   * If the value is null, then GitHub has started a background job to compute the mergeability.
+   */
+  mergeable: boolean | null;
 
   rebaseable: boolean;
 
   mergeableState: 'clean' | 'dirty' | 'unstable';
+
+  commits: number;
+
+  comments: number;
+
+  changedFiles: number;
 
 };
