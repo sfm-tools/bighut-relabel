71ea0f967b66e0b84fad9c9f1053e210b3fd4133
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-10-24 01:35:27 +0300

    WhenContainsConflicts - Using DefaultConditionOptions.
    Otherwise, we cannot add other conditions.

--- src/Conditions/WhenContainsConflicts.ts
+++ src/Conditions/WhenContainsConflicts.ts
@@ -1,13 +1,23 @@
+import { DefaultConditionOptions } from '../ConditionOptions';
+import { NotSupportedParameterError } from '../Errors';
 import { LabelerContext } from '../LabelerContext';
 import { BaseCondition } from './BaseCondition';
 
-export class WhenContainsConflicts extends BaseCondition<boolean> {
+export class WhenContainsConflicts extends BaseCondition<boolean, DefaultConditionOptions> {
 
-  constructor(hasConflicts: boolean) {
-    super(hasConflicts);
+  constructor(hasConflicts: boolean, options?: DefaultConditionOptions) {
+    super(hasConflicts, options);
   }
 
   public async test(context: LabelerContext): Promise<boolean> {
+    const {
+      noOne,
+    } = this.getOptions();
+
+    if (noOne) {
+      throw new NotSupportedParameterError('noOne');
+    }
+
     const {
       mergeableState,
     } = await context.pullRequest.statusInfo;
