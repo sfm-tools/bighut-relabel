d42893a625b1b312da67f8990f83e56b4fd74278
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-10-24 01:38:19 +0300

    WhenContainsConflictsTest - Added test for NotSupportedParameterError.

--- tests/Conditions/WhenContainsConflictsTest.ts
+++ tests/Conditions/WhenContainsConflictsTest.ts
@@ -1,8 +1,14 @@
-import { expect } from 'chai';
+import { expect, should, use } from 'chai';
+import chaiAsPromised from 'chai-as-promised';
 
+import { DefaultConditionOptions } from '../../src/ConditionOptions';
 import { WhenContainsConflicts } from '../../src/Conditions';
+import { NotSupportedParameterError } from '../../src/Errors';
 import { LabelerContext } from '../../src/LabelerContext';
 import { pullRequests } from '../Resources';
+
+use(chaiAsPromised);
+should();
 
 describe('WhenContainsConflicts', () => {
 
@@ -36,6 +42,17 @@ describe('WhenContainsConflicts', () => {
     const result = await when.test(context);
 
     expect(result).to.be.false;
+  });
+
+  it('an exception should be thrown', () => {
+    const options = new DefaultConditionOptions(null);
+    const when = new WhenContainsConflicts(true, options);
+
+    options.noOne();
+
+    const context = new LabelerContext(pullRequests[0]);
+
+    return when.test(context).should.be.rejectedWith(new NotSupportedParameterError('noOne').message);
   });
 
 });
