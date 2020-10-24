d2eee7191e7f7dc2c2b1ae8feb68dd8d89297612
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-10-25 02:31:16 +0300

    #9 BaseActionExecutor - Added properties: linked and executed.

--- src/ActionExecutors/BaseActionExecutor.ts
+++ src/ActionExecutors/BaseActionExecutor.ts
@@ -1,23 +1,39 @@
 import { BaseAction } from '../Actions';
+import { IAction } from '../Interfaces';
 import { LabelerContext } from '../LabelerContext';
 import { IActionExecutor } from './IActionExecutor';
 
-export abstract class BaseActionExecutor<TAction extends BaseAction> implements IActionExecutor<TAction> {
+export abstract class BaseActionExecutor<TAction extends IAction> implements IActionExecutor<TAction> {
 
-  private readonly _action: TAction = null;
+  private readonly _action: TAction;
+
+  private readonly _linked: IActionExecutor;
+
+  private _executed: boolean = false;
 
   public get action(): TAction {
     return this._action;
   }
 
-  constructor(action: TAction) {
+  public get linked(): IActionExecutor {
+    return this._linked;
+  }
+
+  public get executed(): boolean {
+    return this._executed;
+  }
+
+  constructor(action: TAction, linked: IActionExecutor) {
     this._action = action;
+    this._linked = linked;
   }
 
   protected async canExecute(context: LabelerContext): Promise<boolean> {
+    const action = this.action as unknown as BaseAction;
+
     let result: boolean = true;
 
-    for (const condition of this.action['_conditions']) {
+    for (const condition of action.conditions) {
       result = await condition.test(context);
 
       if (!result) {
