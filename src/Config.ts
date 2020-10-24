a8423399a9ace84e2ffc0a76fa82e6a06975ed6b
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-10-25 02:38:31 +0300

    #9 Config - Using interfaces and implemented linking executors.

--- src/Config.ts
+++ src/Config.ts
@@ -1,8 +1,8 @@
 import {
   AddCommentActionExecutor,
   AddLabelActionExecutor,
-  BaseActionExecutor,
   ExecuteActionExecutor,
+  IActionExecutor,
   RemoveLabelActionExecutor,
   SetDescriptionActionExecutor,
   SetMilestoneActionExecutor,
@@ -18,71 +18,93 @@ import {
   SetMilestoneAction,
   SetTitleAction,
 } from './Actions';
+import {
+  IAction,
+  IActionCollection,
+  IConfig,
+  ILinkingActions,
+} from './Interfaces';
 import { LabelerContext } from './LabelerContext';
 import { TaskFunction } from './Types';
 
-export class Config {
+export class Config implements IConfig, IActionCollection, ILinkingActions {
 
-  private _actions = new Array<BaseActionExecutor<BaseAction>>();
+  public readonly actions = new Array<IActionExecutor>();
 
-  public addComment(text: string): BaseAction {
+  public linkingMode: boolean = false;
+
+  public addComment(text: string): IAction {
     return this.addAction(
       new AddCommentActionExecutor(
-        new AddCommentAction(text)
+        new AddCommentAction(text, this),
+        this.getLink()
       )
     );
   }
 
-  public addLabel(label: string): BaseAction {
+  public addLabel(label: string): IAction {
     return this.addAction(
       new AddLabelActionExecutor(
-        new AddLabelAction(label)
+        new AddLabelAction(label, this),
+        this.getLink()
       )
     );
   }
 
-  public removeLabel(label: string): BaseAction {
+  public removeLabel(label: string): IAction {
     return this.addAction(
       new RemoveLabelActionExecutor(
-        new RemoveLabelAction(label)
+        new RemoveLabelAction(label, this),
+        this.getLink()
       )
     );
   }
 
-  public setTitle(title: string | { (title: string, context?: LabelerContext): string }): BaseAction {
+  public setTitle(title: string | { (title: string, context?: LabelerContext): string }): IAction {
     return this.addAction(
       new SetTitleActionExecutor(
-        new SetTitleAction(title)
+        new SetTitleAction(title, this),
+        this.getLink()
       )
     );
   }
 
-  public setDescription(description: string | { (description: string, context?: LabelerContext): string }): BaseAction {
+  public setDescription(description: string | { (description: string, context?: LabelerContext): string }): IAction {
     return this.addAction(
       new SetDescriptionActionExecutor(
-        new SetDescriptionAction(description)
+        new SetDescriptionAction(description, this),
+        this.getLink()
       )
     );
   }
 
-  public setMilestone(milestoneName: string | { (milestoneName: string, context?: LabelerContext): string }): BaseAction {
+  public setMilestone(milestoneName: string | { (milestoneName: string, context?: LabelerContext): string }): IAction {
     return this.addAction(
       new SetMilestoneActionExecutor(
-        new SetMilestoneAction(milestoneName)
+        new SetMilestoneAction(milestoneName, this),
+        this.getLink()
       )
     );
   }
 
-  public execute(action: TaskFunction): ExecuteAction {
+  public execute(action: TaskFunction): IAction {
     return this.addAction(
       new ExecuteActionExecutor(
-        new ExecuteAction(action)
+        new ExecuteAction(action, this),
+        this.getLink()
       )
     );
   }
 
-  private addAction<TAction extends BaseAction, TActionExecutor extends BaseActionExecutor<TAction>>(executorInstance: TActionExecutor): TAction {
-    this._actions.push(executorInstance);
+  private getLink(): IActionExecutor {
+    return this.linkingMode
+      ? this.actions[this.actions.length - 1]
+      : null;
+  }
+
+  private addAction<TAction extends BaseAction, TActionExecutor extends IActionExecutor<TAction>>(executorInstance: TActionExecutor): TAction {
+    this.actions.push(executorInstance);
+    this.linkingMode = false;
 
     return executorInstance.action;
   }
