cf68b79b24f6d84337bec0f9344da7fc01f049a8
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-10-24 19:31:50 +0300

    StringConditionOptionsValues - deprecated

--- src/ConditionOptions/Values/StringConditionOptionsValues.ts
+++ src/ConditionOptions/Values/StringConditionOptionsValues.ts
@@ -1,6 +1,9 @@
 import { DefaultPredicateType } from '../../Types';
 import { ConditionOptionsValues } from './ConditionOptionsValues';
 
+/**
+ * @deprecated It looks like these conditions are redundant.
+ */
 export type StringConditionOptionsValues = ConditionOptionsValues & {
 
   exclude?: Array<DefaultPredicateType>;
