6f87317fbd04f5598b83ecdc9a819dd6ded527b0
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-10-25 02:22:53 +0300

    #9 IActionExecutor - Added the linked property.

--- src/ActionExecutors/IActionExecutor.ts
+++ src/ActionExecutors/IActionExecutor.ts
@@ -1,9 +1,12 @@
 import { BaseAction } from '../Actions';
+import { IAction } from '../Interfaces';
 import { LabelerContext } from '../LabelerContext';
 
-export interface IActionExecutor<TAction extends BaseAction = BaseAction> {
+export interface IActionExecutor<TAction extends IAction = BaseAction> {
 
   readonly action: TAction;
+
+  readonly linked: IActionExecutor;
 
   readonly executed: boolean;
 
