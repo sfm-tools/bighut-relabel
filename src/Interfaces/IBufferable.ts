cd7efd07d763b98e577f83654be72e93e2a81f2c
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-10-25 16:44:35 +0300

    #12 Created IBufferable.

---
+++ src/Interfaces/IBufferable.ts
@@ -0,0 +1,8 @@
+export interface IBufferable {
+
+  /**
+   * Clears buffers and causes buffered data to be written.
+   */
+  flush(): void;
+
+}
--- src/Interfaces/index.ts
+++ src/Interfaces/index.ts
@@ -1,6 +1,7 @@
 export { IAction } from './IAction';
 export { IActionCollection } from './IActionCollection';
 export { IActionExecutor } from './IActionExecutor';
+export { IBufferable } from './IBufferable';
 export { IConfig } from './IConfig';
 export { ILinkingActions } from './ILinkingActions';
 export { ILogger } from './ILogger';
