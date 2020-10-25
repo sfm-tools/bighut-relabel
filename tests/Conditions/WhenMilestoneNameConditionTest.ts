674787b05b0424c187dbbe08e1dcd68f1ca8ac81
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-10-25 20:14:52 +0300

    #10 Created tests for WhenMilestoneNameCondition.

---
+++ tests/Conditions/WhenMilestoneNameConditionTest.ts
@@ -0,0 +1,30 @@
+import { expect } from 'chai';
+
+import { WhenMilestoneNameCondition } from '../../src/Conditions';
+import { LabelerContext } from '../../src/LabelerContext';
+import { pullRequests } from '../Resources';
+
+describe('WhenMilestoneNameCondition', () => {
+
+  it('should return true for milestone matching name', (): void => {
+    const context = new LabelerContext(pullRequests[1]);
+    const when = new WhenMilestoneNameCondition('Version 2.0');
+
+    expect(when.test(context)).to.be.true;
+  });
+
+  it('should return false for milestone non-matching name', (): void => {
+    const context = new LabelerContext(pullRequests[1]);
+    const when = new WhenMilestoneNameCondition('Version 3.0');
+
+    expect(when.test(context)).to.be.false;
+  });
+
+  it('should return false when a milestone is null', (): void => {
+    const context = new LabelerContext(pullRequests[0]);
+    const when = new WhenMilestoneNameCondition('Version 2.0');
+
+    expect(when.test(context)).to.be.false;
+  });
+
+});
