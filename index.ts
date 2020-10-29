9f44c9c6ec435aef866d01ada17d2d9aebcd0f7a
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-10-30 00:01:50 +0300

    Exported IConfig, LabelerContext, and TaskContext.

--- index.ts
+++ index.ts
@@ -1,1 +1,4 @@
-export { createConfig, fix,test } from './src';
+export { createConfig, fix, test } from './src';
+export { IConfig } from './src/Interfaces';
+export { LabelerContext } from './src/LabelerContext';
+export { TaskContext } from './src/Types';
