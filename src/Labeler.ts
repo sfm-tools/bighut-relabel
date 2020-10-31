987acb3053ff2eb26c40a4f3bc7c22be111bb4a6
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-10-31 18:10:10 +0300

    #1 Labeler.processPullRequests - Separation of cache for test and fix modes.
    But this still needs to be thought about...

--- src/Labeler.ts
+++ src/Labeler.ts
@@ -139,7 +139,7 @@ export class Labeler implements ILabeler {
         continue;
       }
 
-      const cacheKey = `pr-${pullRequest.code}`;
+      const cacheKey = `pr-${pullRequest.code}/${test === true}`;
 
       if (this._cache.has(cacheKey)) {
         logSkipped(
