97f2d042a636e6ce366c32dabcaf4e0e4bb98072
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-10-31 23:26:23 +0300

    Removed redundant index.ts

--- index.ts
+++
@@ -1,4 +0,0 @@
-export { createConfig, fix, test } from './src';
-export { IConfig } from './src/Interfaces';
-export { LabelerContext } from './src/LabelerContext';
-export { TaskContext } from './src/Types';
