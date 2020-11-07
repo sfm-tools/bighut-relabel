c9b10d7465d4465762abff407b5beb2abd1e3969
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-11-08 00:06:25 +0300

    #18 Helpers - Added emptyCacheableAction function.

--- tests/Resources/Helpers.ts
+++ tests/Resources/Helpers.ts
@@ -1,5 +1,9 @@
 import { CacheableAction } from '../../src/CacheableAction';
 
+export function emptyCacheableAction<T>(): CacheableAction<T> {
+  return new CacheableAction(() => Promise.resolve<T>(undefined));
+}
+
 export function emptyListOfCacheableAction<T>(): CacheableAction<Array<T>> {
   return new CacheableAction(() => Promise.resolve<Array<T>>([]));
 }
