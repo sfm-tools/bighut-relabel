72e8ae49d887bbc2266df90297219f336d6d2059
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-11-08 17:00:30 +0300

    #18 Created RemoveRequestedReviewersAction.

--- src/Actions/index.ts
+++ src/Actions/index.ts
@@ -3,6 +3,7 @@ export { AddLabelAction } from './AddLabelAction';
 export { BaseAction } from './BaseAction';
 export { ExecuteAction } from './ExecuteAction';
 export { RemoveLabelAction } from './RemoveLabelAction';
+export { RemoveRequestedReviewersAction } from './RemoveRequestedReviewersAction';
 export { RequestReviewersAction } from './RequestReviewersAction';
 export { SetDescriptionAction } from './SetDescriptionAction';
 export { SetMilestoneAction } from './SetMilestoneAction';
