15b86b761b42fac6bd248999a2b555cdf9a18207
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-11-08 12:49:47 +0300

    Additional test for SetMilestoneActionExecutor.

--- tests/ActionExecutors/SetMilestoneActionExecutorTest.ts
+++ tests/ActionExecutors/SetMilestoneActionExecutorTest.ts
@@ -25,6 +25,32 @@ describe('SetMilestoneActionExecutor', () => {
     expect(updater.milestone.value).to.equal(action.milestoneName);
   });
 
+  it('should add a task to set milestone using function', async(): Promise<void> => {
+    const context = new LabelerContext({
+      pullRequest: firstPullRequest,
+      test: true,
+    });
+
+    const updater = context.updater;
+    const action = new SetMilestoneAction(
+      (milestone: string): string => milestone || 'Awesome Milestone',
+      null
+    );
+
+    const executor = new SetMilestoneActionExecutor(action, null);
+
+    action
+      .whenAuthorLogin(flossTomUser.login);
+
+    await executor.execute(context);
+
+    expect(updater.milestone.value)
+      .to
+      .equal(
+        action.getMilestoneName(firstPullRequest.milestone?.name, context)
+      );
+  });
+
   it('should not add a task to set milestone', async(): Promise<void> => {
     const context = new LabelerContext({
       pullRequest: firstPullRequest,
