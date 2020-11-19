9e28dbb868255e34786dffcdcb0188192a42eda9
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-11-19 23:55:17 +0300

    #26 BaseCondition.testStringValue - Added debug info. Fixed bug with global regExp.

--- src/Conditions/BaseCondition.ts
+++ src/Conditions/BaseCondition.ts
@@ -33,25 +33,43 @@ export abstract class BaseCondition<
   public abstract test(context: LabelerContext): Promise<boolean> | boolean;
 
   protected testStringValue(value: string, context: LabelerContext, predicate?: TPredicate): boolean {
+    let result: boolean = false;
+
     predicate = predicate || this.predicate;
 
     if (typeof predicate === 'string') {
-      return this.stringComparer(predicate, value);
+      result = this.stringComparer(predicate, value);
+    } else if (typeof predicate === 'function') {
+      result = predicate(value, context);
+    } else if (predicate instanceof RegExp) {
+      // When a regex has the global flag set, test() will advance the lastIndex of the regex.
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test#Using_test()_on_a_regex_with_the_global_flag
+      predicate.lastIndex = 0;
+      result = predicate.test(value);
+    } else if (Array.isArray(predicate)) {
+      result = predicate.includes(value);
     }
 
-    if (typeof predicate === 'function') {
-      return predicate(value, context);
-    }
+    context.logger.debug(
+      '{class}.{method}: pr#{pullRequest.code}: the value "{value}" testing with predicate {type} yields the result "{result}"',
+      {
+        class: this.constructor.name,
+        method: this.test.name,
+        value,
+        type: typeof predicate,
+        ...(
+          predicate instanceof RegExp
+            ? {
+              regExp: predicate
+            }
+            : {}
+        ),
+        result,
+        pullRequest: context.pullRequest,
+      }
+    );
 
-    if (predicate instanceof RegExp) {
-      return predicate.test(value);
-    }
-
-    if (Array.isArray(predicate)) {
-      return predicate.includes(value);
-    }
-
-    throw new Error('The predicate type is not supported. Expectected a string, Array<string>, RegExp or Function.');
+    return result;
   }
 
   /**
