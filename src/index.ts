980ac75c8c6014cde75aca0d7280e9e294e38989
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-11-21 01:11:46 +0300

    Exported Auth, GitHubClient, and IApiProviderClient.

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { LabelerOptions } from './Types';
 export { IConfig } from './Interfaces';
 export { LabelerContext } from './LabelerContext';
 export { TaskContext } from './Types'; // TODO: Remove in future releases
+export { Auth, GitHubClient, IApiProviderClient };
 
 type BaseRepositoryOptions = {
 
