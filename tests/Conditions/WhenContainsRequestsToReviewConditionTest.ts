90bb14ae82d8400eaf8c813ddd3c3346f354c28b
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-12-05 01:25:02 +0300

    #31 Improved tests for WhenContainsRequestsToReviewCondition.

--- tests/Conditions/WhenContainsRequestsToReviewConditionTest.ts
+++ tests/Conditions/WhenContainsRequestsToReviewConditionTest.ts
@@ -1,9 +1,9 @@
 import { expect } from 'chai';
 
-import { DefaultConditionOptions } from '../../src/ConditionOptions';
+import { WhenContainsRequestsToReviewConditionOptions } from '../../src/ConditionOptions';
 import { WhenContainsRequestsToReviewCondition } from '../../src/Conditions';
 import { LabelerContext } from '../../src/LabelerContext';
-import { firstPullRequest, noReviewPullRequest } from '../Resources';
+import { firstPullRequest, flossMotUser, loftSomsUser, msSoftLoUser, noReviewPullRequest } from '../Resources';
 
 describe('WhenContainsRequestsToReviewCondition', () => {
 
@@ -41,7 +41,7 @@ describe('WhenContainsRequestsToReviewCondition', () => {
   });
 
   it('should return true for a pull request that contains requests to reviewers when reviewers are expected and used the "nothing" option', (): void => {
-    const options = new DefaultConditionOptions(null);
+    const options = new WhenContainsRequestsToReviewConditionOptions(null);
     const when = new WhenContainsRequestsToReviewCondition(true, options);
 
     options.nothing();
@@ -53,6 +53,97 @@ describe('WhenContainsRequestsToReviewCondition', () => {
     const result = when.test(context);
 
     expect(result).to.be.false;
+  });
+
+  it('should return true for a pull request that contains requests to one of the reviewers when reviewers are expected', (): void => {
+    const options = new WhenContainsRequestsToReviewConditionOptions(null);
+    const when = new WhenContainsRequestsToReviewCondition(true, options);
+    const context = new LabelerContext({
+      pullRequest: noReviewPullRequest,
+      test: true,
+    });
+
+    options.oneOf([
+      flossMotUser.login,
+      loftSomsUser.login,
+    ]);
+
+    const result = when.test(context);
+
+    expect(result).to.be.true;
+  });
+
+  it('should return false for a pull request that no contains requests to a specific one of the reviewers when reviewers are expected', (): void => {
+    const options = new WhenContainsRequestsToReviewConditionOptions(null);
+    const when = new WhenContainsRequestsToReviewCondition(true, options);
+    const context = new LabelerContext({
+      pullRequest: firstPullRequest,
+      test: true,
+    });
+
+    options.oneOf([
+      flossMotUser.login,
+    ]);
+
+    const result = when.test(context);
+
+    expect(result).to.be.false;
+  });
+
+  it('should return true for a pull request that contains requests to specific reviewers when reviewers are expected', (): void => {
+    const options = new WhenContainsRequestsToReviewConditionOptions(null);
+    const when = new WhenContainsRequestsToReviewCondition(true, options);
+    const context = new LabelerContext({
+      pullRequest: noReviewPullRequest,
+      test: true,
+    });
+
+    options.all([
+      flossMotUser.login,
+      loftSomsUser.login,
+    ]);
+
+    const result = when.test(context);
+
+    expect(result).to.be.true;
+  });
+
+  it('should return false for a pull request that contains requests to specific reviewers when reviewers are expected and one is missing', (): void => {
+    const options = new WhenContainsRequestsToReviewConditionOptions(null);
+    const when = new WhenContainsRequestsToReviewCondition(true, options);
+    const context = new LabelerContext({
+      pullRequest: noReviewPullRequest,
+      test: true,
+    });
+
+    options.all([
+      flossMotUser.login,
+      msSoftLoUser.login,
+    ]);
+
+    const result = when.test(context);
+
+    expect(result).to.be.false;
+  });
+
+  it('should return true for a pull request that contains requests to specific reviewers when reviewers are expected and one is missing with the "nothing" option', (): void => {
+    const options = new WhenContainsRequestsToReviewConditionOptions(null);
+    const when = new WhenContainsRequestsToReviewCondition(true, options);
+    const context = new LabelerContext({
+      pullRequest: noReviewPullRequest,
+      test: true,
+    });
+
+    options.all([
+      flossMotUser.login,
+      msSoftLoUser.login,
+    ]);
+
+    options.nothing();
+
+    const result = when.test(context);
+
+    expect(result).to.be.true;
   });
 
 });
