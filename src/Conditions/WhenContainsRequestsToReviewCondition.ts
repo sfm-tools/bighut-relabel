c9879f013aa2c012961f15b68977a4beb39486ec
alexeyclearscale <alexey.nemiro@clearscale.com>
2020-12-05 01:24:47 +0300

    #31 WhenContainsRequestsToReviewCondition - Using WhenContainsRequestsToReviewConditionOptions.

--- src/Conditions/WhenContainsRequestsToReviewCondition.ts
+++ src/Conditions/WhenContainsRequestsToReviewCondition.ts
@@ -1,17 +1,53 @@
-import { DefaultConditionOptions } from '../ConditionOptions';
+import { User } from '../ApiProviders';
+import { WhenContainsRequestsToReviewConditionOptions } from '../ConditionOptions';
 import { LabelerContext } from '../LabelerContext';
 import { BaseCondition } from './BaseCondition';
 
-export class WhenContainsRequestsToReviewCondition extends BaseCondition<boolean, DefaultConditionOptions> {
+export class WhenContainsRequestsToReviewCondition extends BaseCondition<boolean, WhenContainsRequestsToReviewConditionOptions> {
 
-  constructor(hasConflicts: boolean, options?: DefaultConditionOptions) {
+  constructor(hasConflicts: boolean, options?: WhenContainsRequestsToReviewConditionOptions) {
     super(hasConflicts, options);
   }
 
   public test(context: LabelerContext): boolean {
     const {
+      all,
+      oneOf,
+      value,
+    } = this.getOptions(context);
+
+    const users = [...value || []];
+    const hasUsers = !!users.length;
+
+    const {
       requestedReviewers,
     } = context.pullRequest;
+
+    if (hasUsers) {
+      if (oneOf) {
+        if (
+          requestedReviewers.filter(
+            (user: User): boolean => (
+              users.includes(user.login)
+            )
+          ).length === 0
+        ) {
+          return this.testResult(false, context);
+        }
+      }
+
+      if (all) {
+        if (
+          !requestedReviewers.every(
+            (user: User): boolean => (
+              users.includes(user.login)
+            )
+          )
+        ) {
+          return this.testResult(false, context);
+        }
+      }
+    }
 
     if (this.predicate) {
       return this.testResult(
